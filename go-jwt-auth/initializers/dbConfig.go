3973ec16a280b533ffedd9dc7d512952b9d0e4c0
irfan-arrosid <irfanarrosid4@gmail.com>
2023-03-12 21:29:54 +0700

    Create environment variable using godotenv and connect to database using gorm

---
+++ go-jwt-auth/initializers/dbConfig.go
@@ -0,0 +1,23 @@
+package initializers
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func DbConnect() {
+	db, err := gorm.Open(postgres.Open(os.Getenv("DB_URL")), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println("Failed to connect database!")
+	} else {
+		fmt.Println("Database connected....")
+	}
+
+	DB = db
+}
--- go-jwt-auth/main.go
+++ go-jwt-auth/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"GolangApp-mini-projects/go-jwt-auth/initializers"
 
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.DbConnect()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
